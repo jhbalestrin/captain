ad32fb9ea9f6bef7b3ca4562dba5bd659bbbce41
GitHub Action <action@github.com>
2021-09-07 02:37:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/jhbalestrin/captain/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jhbalestrin/captain/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jhbalestrin.captain.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jhbalestrin.captain.MyBundle
 
 class MyApplicationService {
 
